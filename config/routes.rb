ae54094b02844a1d9f817d7d1b22972746106659
Gabriel <gabriel@softwarecriollo.com>
2015-07-14 15:01:37 -0430

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ KepplerBlog::Engine.routes.draw do
   resources :categories
 	 scope :admin, :blog do
   	resources :posts do 
-      get '(page/:page)', action: :index, on: :collection, as: ''
+      get '(page/:page)', action: :index, on: :collection, as: 'search'
       delete '/destroy_multiple', action: :destroy_multiple, on: :collection, as: :destroy_multiple
     end
   end
 
-  scope :admin, :posts do
+  scope :admin, :blog do
     resources :categories do
-      get '(page/:page)', action: :index, on: :collection, as: ''
+      get '(page/:page)', action: :index, on: :collection, as: 'search'
       delete '/destroy_multiple', action: :destroy_multiple, on: :collection, as: :destroy_multiple
     end
   end
--- lib/keppler_blog.rb
+++ lib/keppler_blog.rb
@@ -1,7 +1,7 @@
 require "keppler_blog/engine"
 
 module KepplerBlog
-	ROUTES_TREE_SIDEBAR = { icon: "local_library", title: "blog", routes: { 'keppler_blog/posts' => "/blog/posts", 'keppler_blog/categories' => "blog/posts/categories" }}
+	ROUTES_TREE_SIDEBAR = { icon: "local_library", title: "blog", routes: { 'keppler_blog/posts' => "posts", 'keppler_blog/categories' => "categories" }}
 
 	# true/false si desea que se autentiquen los usuarios para poder usar el modulo en el frontend
   mattr_accessor :widget_twitter_id
