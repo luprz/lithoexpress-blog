89592f7b489c8e7fca665c92378e9307bf526419
Gabriel <gabriel@softwarecriollo.com>
2015-08-03 11:06:25 -0430

    update

--- lib/keppler_blog.rb
+++ lib/keppler_blog.rb
@@ -3,7 +3,7 @@ require "keppler_blog/engine"
 module KepplerBlog
 	ROUTE_SIDEBAR = true
 
-  mattr_accessor :widget_twitter_id, :twitter_username, :facebook_app_id, :posts_pet_page
+  mattr_accessor :widget_twitter_id, :twitter_username, :facebook_app_id, :posts_per_page
 
   def self.setup
     yield self
